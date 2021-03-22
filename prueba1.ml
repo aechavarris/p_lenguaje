e8596299fd2e0b70a346496ca7d6e1a01685588d
Markles <bymarkles@gmail.com>
2021-03-22 13:01:46 +0100

    Las dos primeras pruebas del lexico

---
+++ prueba1.ml
@@ -0,0 +1,13 @@
+%--------------------------------------------------------
+Programa prueba1;
+%--------------------------------------------------------
+
+entero hasta, m, n, ñ;
+
+Principio
+	hasta := 6;
+  Mq (hasta <> 0) 
+		escribir("hasta");
+		hasta := hasta - 1;
+	FMq
+Fin
