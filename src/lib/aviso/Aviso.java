39f891bc352c475764f258e7d6117d252f2dfa1d
aechavarris <56934671+aechavarris@users.noreply.github.com>
2021-04-13 09:52:34 +0200

    Update Aviso.java

--- src/lib/aviso/Aviso.java
+++ src/lib/aviso/Aviso.java
@@ -1,5 +1,13 @@
 package lib.aviso;
 
+import analizador.Token;
+
 public class Aviso {
+
+	public static void deteccion(String string, Token t) {
+		// TODO Auto-generated method stub
+		System.out.println(string);
+		System.out.println("Error en token "+t.toString());
+	}
 
 }
